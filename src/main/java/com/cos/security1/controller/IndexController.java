2a80605bc5aad6f0e9e58ceccb46cba8dc2de926
kimjaebeom98 <woqjadl6487@naver.com>
2023-12-30 20:37:46 +0900

    회원가입 적용

--- src/main/java/com/cos/security1/controller/IndexController.java
+++ src/main/java/com/cos/security1/controller/IndexController.java
@@ -1,11 +1,23 @@
 package com.cos.security1.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.cos.security1.model.User;
+import com.cos.security1.repository.UserRepository;
 
 @Controller
 public class IndexController {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncode;
 	
 	@GetMapping({"", "/"})
 	public String index() {
@@ -27,18 +39,31 @@ public class IndexController {
 		return "manager";
 	}
 	
-	@GetMapping("/login")
-	public @ResponseBody String login() {
-		return "login";
+	@GetMapping("/loginForm")
+	public String loginForm() {
+		return "loginForm";
 	}
 	
-	@GetMapping("/join")
-	public @ResponseBody String join() {
-		return "join";
+	// 회원가입 페이지
+	@GetMapping("/joinForm")
+	public String joinForm() {
+		return "joinForm";
 	}
 	
-	@GetMapping("/joinProc")
-	public @ResponseBody String joinProc() {
-		return "회원가입 완료됨 !!";
+	// 회원가입 처리
+	@PostMapping("/join")
+	public String join(User user) {
+		System.out.println(user);
+		user.setRole("ROLE_USER");
+		// 비밀번호 암호화 !!
+		// 단순 문자열로 저장하면 시큐리티가 작동 못함 이유는 패스워드가 암호화가 안돼서
+		String rawPassword = user.getPassword();
+		String encPassword = bCryptPasswordEncode.encode(rawPassword);
+		user.setPassword(encPassword);
+		
+		userRepository.save(user); 
+		
+		return "redirect:/loginForm";
 	}
+
 }
